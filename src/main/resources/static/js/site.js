0c83c10ffb44169a900b8b5b5e04ed4addbc39a1
Siva <24pa1a0505@vishnu.edu.in>
2026-05-26 20:54:44 +0530

    Toggle Added

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,1 +1,27 @@
 console.log("Hello from JS");
+
+function changeImage(){
+    let image = document.getElementById("myImage");
+
+    if(image.src.includes("/img/img1.png")){
+        image.src = "/img/img2.jpg";
+    }else{
+        image.src = "/img/img1.png";
+    }
+
+    let heading = document.getElementById("heading");
+
+    if(heading.innerHTML === "Virat Kohli"){
+        heading.innerHTML = "King Kohli";
+    }else{
+        heading.innerHTML = "Virat Kohli";
+    }
+
+    let para = document.getElementById("text");
+
+    if(para.innerHTML === "Cricketer(Right-Hand Batsman)<br>2008-2014"){
+        para.innerHTML = "Cricketer(Called as King)<br>2014-2026";
+    }else{
+        para.innerHTML = "Cricketer(Right-Hand Batsman)<br>2008-2014";
+    }
+}
